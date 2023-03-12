c10d2bed2a08a4630ccb3901f0dc09f7e1f19543
kosvish <mr.kocta@bk.ru>
2023-03-12 10:14:15 +0300

    feat(create): font variables, func display digits

--- main.py
+++ main.py
@@ -1,18 +1,45 @@
 import tkinter as tk
 
+# colors
 LIGHT_GRAY = "#F5F5F5"
+LABEL_COLOR = "#25265E"
+
+# fonts
+SMALL_FONT_STYLE = ("Arial", 16)
+LARGE_FONT_STYLE = ("Arial", 40, "bold")
 
 
 class Calculator:
     def __init__(self):
+        # main window
         self.window = tk.Tk()
         self.window.geometry("375x667")
-        self.window.resizable()
+        self.window.resizable(False, False)
         self.window.title("Calculator")
+
+        # meaning of expressions
+        self.total_expression = "0"
+        self.current_expression = "0"
 
         # variables for display
         self.display_frame = self.create_display_frame()
         self.buttons_frame = self.create_buttons_frame()
+
+        self.total_label, self.label = self.create_display_labels()
+
+    def create_display_labels(self):
+        total_label = tk.Label(
+            self.display_frame, text=self.total_expression,
+            anchor=tk.E, bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24,
+            font=SMALL_FONT_STYLE
+        )
+        total_label.pack(expand=True, fill="both")
+        label = tk.Label(
+            self.display_frame, text=self.current_expression,
+            anchor=tk.E, bg=LIGHT_GRAY, fg=LABEL_COLOR, padx=24, font=LARGE_FONT_STYLE
+        )
+        label.pack(expand=True, fill="both")
+        return total_label, label
 
     def create_display_frame(self):
         frame = tk.Frame(self.window, height=221, bg=LIGHT_GRAY)
